2730340e5e58a92c6da3537a5b223fa340e4e9d2
Mutudu <aeondxf@live.com>
2023-12-19 15:22:41 +0800

    Refactor Zaabee.Compressor.Abstractions

--- src/Zaabee.Compressor.Abstractions/ICompressor.Bytes.cs
+++ src/Zaabee.Compressor.Abstractions/ICompressor.Bytes.cs
@@ -1,22 +1,8 @@
 ﻿namespace Zaabee.Compressor.Abstractions;
 
+
 public partial interface ICompressor
 {
     byte[] Compress(byte[] rawBytes);
     byte[] Decompress(byte[] compressedBytes);
-}
-
-public static class CompressorExtensions
-{
-    public static byte[] Compress(
-        this ICompressor compressor,
-        string str,
-        Encoding? encoding = null
-    ) => compressor.Compress(str.GetBytes(encoding ?? Consts.DefaultEncoding));
-
-    public static string DecompressToString(
-        this ICompressor compressor,
-        byte[] bytes,
-        Encoding? encoding = null
-    ) => compressor.Decompress(bytes).GetString(encoding ?? Consts.DefaultEncoding);
 }
